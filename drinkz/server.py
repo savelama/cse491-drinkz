2a5452de18e0b0e386a02517a1efc1c9ecc83a2c
Matthew Savela <savelama@arctic.cse.msu.edu>
2013-05-01 20:56:56 -0400

    HW6 - Completed step 1

--- drinkz/server.py
+++ drinkz/server.py
@@ -3,6 +3,7 @@ import random
 import socket
 import time
 import app
+from StringIO import StringIO
 
 #Create a socket
 s = socket.socket()
@@ -30,9 +31,50 @@ while True:
 
     # include the directories that are acceptable for the client to enter
     data = c.recv(1024)
-    if data[:3] == "GET":
+
+    while "\r\n\r\n" not in data:
+        receivedData = c.recv(1024)
+        if not receivedData:
+            break
+        data += receivedData
+
+    if data[:4] == "POST":
+        splitLines = data.splitlines()
+        if(len(splitLines) < 1):
+            print "Error"
+            continue
+        firstLine = splitLines[0]
+
+        reqType, reqPath, reqProtocol = firstLine.split()
+
+        reqHeader = splitLines[1:]
+
+        # SET ENVIRON VARIABLES
+        environ = {}
+        environ['PATH_INFO'] = reqPath
+        environ['REQUEST_METHOD'] = reqType
+        environ['CONTENT_LENGTH'] = len(reqHeader[-1])
+        environ['wsgi.input'] = StringIO(reqHeader[-1])
+
+        results = app_obj(environ, my_start_response);
+
+        responseHeaders = []
+        for key, value in d['headers']:
+            responseHeaders.append(key + ": " + value)
+
+        status = "HTTP/1.0 "
+        status += d['status']
+        status += "\r\n"
+
+        response = "\r\n".join(responseHeaders) + "\r\n\r\n" + "".join(results)
+
+        c.send(status)
+        c.send(response)
+
+    elif data[:3] == "GET":
         status = "HTTP/1.0 "
         environ = {}
+
         if '/recipes' in data[4:]:
             environ['PATH_INFO'] = '/recipes'
         elif '/inventory' in data[4:]:
@@ -47,6 +89,14 @@ while True:
             environ['PATH_INFO'] = '/add_liquor_type'
         elif '/add_to_inventory' in data[4:]:
             environ['PATH_INFO'] = '/add_to_inventory'
+        elif '/login_1' in data[4:]:
+            environ['PATH_INFO'] = '/login_1'
+        elif '/login1_process' in data[4:]:
+            environ['PATH_INFO'] = '/login1_process'
+        elif '/logout' in data[4:]:
+            environ['PATH_INFO'] = '/logout'
+        elif '/status' in data[4:]:
+            environ['PATH_INFO'] = '/status'
         elif '/' in data[5:]:
             environ['PATH_INFO'] = '/'
         else:
